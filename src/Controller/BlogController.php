7dc644364d9b6a621c22c5958c4e0a88c023bf4c
Guiderdoni Bastien <beegeebuzz@hotmail.fr>
2019-07-01 16:47:56 +0200

    Add show method

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,16 +8,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-
+/**
+ * Class BlogController
+ * @Route("/blog", name="blog_")
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'owner' => 'Bastien',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, defaults={"slug"= "article-sans-titre"}, name="show")
+     */
+    public function show($slug):Response
+    {
+        $slug = ucwords(str_replace("-", " ", $slug));
+        return $this->render('blog/show.html.twig', [
+            'slug' => $slug
         ]);
     }
 }
